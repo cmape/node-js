1efe8758e49f6f11497dbfefe9969c8a781eaa67
Cmape <carlosmape69@gmail.com>
2022-04-26 19:33:36 -0500

    actualizacion 1

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 const express = require('express')
 const app = express()
 
-let nombre
+let Id, Firstname, Lastname, Old;
 
 app.use(express.json());
 
-app.post('/', function(request, response){
-    nombre= request.body.name
-  console.log(request.body);      // your JSON
-   response.send(nombre);    // echo the result back
-});
-
 app.get('/', function (req, res) {
-  res.send('Hello World')
+    res.send('Hello World')
 })
+  
+app.get('/Id', function (req, res) {
+    res.send(Id)
+    console.log(Id);
+})
+
+app.get('/Firstname', function (req, res) {
+    res.send(Firstname)
+    console.log(Firstname);
+})
+app.get('/Lastname', function (req, res) {
+    res.send(Lastname)
+    console.log(Lastname);
+})
+
+app.get('/Old', function (req, res) {
+    res.send(Old)
+    console.log(Old);
+})
+
+app.post('/', function(request, response){
+  Id = request.body.Id
+  Firstname = request.body.Firstname
+  Lastname = request.body.Lastname
+  Old = request.body.Old
+  console.log(request.body);      // your JSON
+  response.send('Exitoso');    // echo the result back
+});
 
 app.listen(3000)
